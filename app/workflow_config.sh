99d6e1fe3e3ef5cf00d5e7fc750ac097269f894c
mkrause <maikelkrause@gmail.com>
2014-03-16 18:02:34 +0100

    Rename application management tool to 'workflow'.

---
+++ app/workflow_config.sh
@@ -0,0 +1,18 @@
+config_dist_files=(
+    'app/workflow_params.sh.dist'
+    'app/config/parameters.php.dist'
+)
+
+config_remote_host='mkrause.nl'
+config_remote_username='root'
+config_remote_path='/var/www/mkrause.nl/public_html/test'
+
+config_open() {
+    # Needs to be defined locally
+    return
+}
+
+# Import local params
+if [ -f "${script_dir}/workflow_params.sh" ]; then
+    . "${script_dir}/workflow_params.sh"
+fi
