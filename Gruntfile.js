7f705a9e84665ec0c875ff41a419c61dac67fabb
mkrause <maikelkrause@gmail.com>
2014-03-18 18:25:03 +0100

    Add Gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        mainJsPath: "web/scripts/main.js",
+        mainJsBuildPath: "web/build/main.js",
+        mainCssPath: "web/style/main.css",
+        mainCssBuildPath: "web/build/main.css",
+        requirejs: {
+            js: {
+                options: {
+                    name: "main",
+                    baseUrl: "web/scripts",
+                    mainConfigFile: "web/scripts/require_config.js",
+                    out: "<%= mainJsBuildPath %>",
+                    // Don't optimize yet, we want to run ngmin first
+                    optimize: "none"
+                }
+            },
+            css: {
+                options: {
+                    cssIn: "<%= mainCssPath %>",
+                    out: "<%= mainCssBuildPath %>"
+                }
+            }
+        },
+        ngmin: {
+            buildMain: {
+                src: ["<%= mainJsBuildPath %>"],
+                dest: "<%= mainJsBuildPath %>"
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            buildMain: {
+                src: "<%= mainJsBuildPath %>",
+                dest: "<%= mainJsBuildPath %>"
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    // paths: ["assets/css"]
+                },
+                files: {
+                    "web/build/main.css": "web/style/main.less"
+                }
+            },
+            production: {
+                options: {
+                    strictImports: true,
+                    paths: [
+                        "web/style",
+                        "web/vendor/bootstrap/dist/css",
+                        "web/vendor/font-awesome/css"
+                    ],
+                    cleancss: true,
+                    modifyVars: {
+                        // imgPath: '"http://mycdn.com/path/to/images"',
+                        // bgColor: 'red'
+                    }
+                },
+                files: {
+                    "web/build/main.css": "web/style/main.less"
+                }
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-ngmin');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    
+    // Tasks
+    grunt.registerTask('build:js', ['requirejs:js', 'ngmin', 'uglify']);
+    grunt.registerTask('build:css', ['requirejs:css']);
+    grunt.registerTask('default', ['build:js']);
+};
