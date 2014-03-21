6115810d1c0d509dc1698f9d8ca3e1c62b44512d
mkrause <maikelkrause@gmail.com>
2014-03-21 02:01:34 +0100

    Fix syntax error.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
                     almond: true
                     // TODO
                 }
-            }
+            },
             css: {
                 options: {
                     cssIn: "<%= mainCssPath %>",
